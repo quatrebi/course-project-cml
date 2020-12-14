88f01069a48dd9de554c65b0a1c5f824641ebeea
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-12-14 14:20:10 +0300

    updated

--- js/xmlLoader.js
+++ js/xmlLoader.js
@@ -1,37 +1,24 @@
-function loadXMLDoc(filename)
-{
-if (window.ActiveXObject)
-  {
-  xhttp = new ActiveXObject("Msxml2.XMLHTTP");
-  }
-else 
-  {
-  xhttp = new XMLHttpRequest();
-  }
-xhttp.open("GET", filename, false);
-try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
-xhttp.send("");
-return xhttp.responseXML;
+let xhttp;
+
+function loadXMLDocument (filename) {
+  xhttp = window.ActiveXObject ? new ActiveXObject("Msxml2.XMLHTTP") : new XMLHttpRequest();
+  xhttp.open("GET", filename, false);
+  try { xhttp.responseType = "ms-stream"; }
+  catch (err) {}
+  xhttp.send('');
+  return xhttp.responseXML;
 }
 
-function displayResult(name)
-{
-xml = loadXMLDoc(`xml/${name}.xml`);
-xsl = loadXMLDoc(`xml/xslt/${name}.xslt`);
-console.log(xml);
-console.log(xsl);
-// code for IE
-if (window.ActiveXObject || xhttp.responseType == "msxml-document")
-  {
-  ex = xml.transformNode(xsl);
-  document.innerHTML += ex;
+function getNodeFromXML (filename, parent = "body") {
+  let xml = loadXMLDocument(`xml/${filename}.xml`);
+  let xslt =loadXMLDocument(`xml/xslt/${filename}.xslt`);
+
+  if (window.ActiveXObject || xhttp.responseType === "ms-stream") {
+    document.querySelector(parent).innerHTML += xml.transformNode(xslt);
   }
-// code for Chrome, Firefox, Opera, etc.
-else if (document.implementation && document.implementation.createDocument)
-  {
-  xsltProcessor = new XSLTProcessor();
-  xsltProcessor.importStylesheet(xsl);
-  resultDocument = xsltProcessor.transformToFragment(xml, document);
-  document.body.appendChild(resultDocument);
+  else if (document.implementation && document.implementation.createDocument) {
+    xsltProc = new XSLTProcessor();
+    xsltProc.importStylesheet(xslt);
+    document.querySelector(parent).appendChild(xsltProc.transformToFragment(xml, document));
   }
 }
