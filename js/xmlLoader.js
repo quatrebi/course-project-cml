9574a0cac9d26e5100f84632932f9d1f52426661
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-12-14 20:35:20 +0300

    updated

--- js/xmlLoader.js
+++ js/xmlLoader.js
@@ -9,20 +9,42 @@ function loadXMLDocument (filename) {
   return xhttp.responseXML;
 }
 
+function mergeXMLs (mainFile, mergeFiles = []) {
+  let xml = loadXMLDocument(`xml/${mainFile}.xml`);
+  for (let i = 0; i < mergeFiles.length; i++) {
+    let merge = loadXMLDocument(`xml/${mergeFiles[i]}.xml`);
+    xml.querySelector(mergeFiles[i].toUpperCase()).outerHTML = merge.documentElement.innerHTML;
+  }
+  return xml;
+}
+
+function getNode (xml, xslt, parent = "body") {
+  if (window.ActiveXObject || xhttp.responseType === "ms-stream") {
+    document.querySelector(parent).innerHTML += xml.transformNode(xslt);
+  }
+  else if (document.implementation && document.implementation.createDocument) {
+    let xsltProc = new XSLTProcessor();
+    xsltProc.importStylesheet(xslt);
+    document.querySelector(parent).appendChild(xsltProc.transformToFragment(xml, document));
+  }
+}
+
 function getNodeFromXML (filename, parent = "body") {
   getNodeFromXMLwXSLT(filename, filename, parent);
 }
 
 function getNodeFromXMLwXSLT (xmlName, xsltName, parent = "body") {
   let xml = loadXMLDocument(`xml/${xmlName}.xml`);
-  let xslt =loadXMLDocument(`xml/xslt/${xsltName}.xslt`);
+  let xslt = loadXMLDocument(`xml/xslt/${xsltName}.xslt`);
+  getNode(xml, xslt, parent);
+}
 
-  if (window.ActiveXObject || xhttp.responseType === "ms-stream") {
-    document.querySelector(parent).innerHTML += xml.transformNode(xslt);
-  }
-  else if (document.implementation && document.implementation.createDocument) {
-    xsltProc = new XSLTProcessor();
-    xsltProc.importStylesheet(xslt);
-    document.querySelector(parent).appendChild(xsltProc.transformToFragment(xml, document));
-  }
+function getNodeFromXMLwMerge (mainFile, mergeFiles = [], parent = "body") {
+  let xslt = loadXMLDocument(`xml/xslt/${mainFile}.xslt`);
+  getNode(mergeXMLs(mainFile, mergeFiles), xslt, parent);
+}
+
+function getNodeFromXMLwXSLTwMerge (mainFile, xsltName, mergeFiles = [], parent = "body") {
+  let xslt = loadXMLDocument(`xml/xslt/${xsltName}.xslt`);
+  getNode(mergeXMLs(mainFile, mergeFiles), xslt, parent);
 }
